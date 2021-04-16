0929527e71377ceac2adaab6e9553f78e1bd4cec
quinaiton <quinaiton@hotmail.com>
2021-04-16 16:02:44 -0700

    Fresh Next App With Scss

---
+++ components/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div className>
+      <ul>
+        <li>Logo</li>
+        <div>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Projects</li>
+        </div>
+        <div>
+          <li>Github</li>
+          <li>Linkedin</li>
+          <li>Contact Me</li>
+        </div>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.scss';
+import Nav from '../components/Nav';
+const Index = () => {
+  return (
+    <div className={styles.container}>
+      <Nav />
+    </div>
+  );
+};
+
+export default Index;
