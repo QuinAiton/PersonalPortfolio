636aa1d7d255622ff454ff73c59ee1eed15cfb0a
quinaiton <quinaiton@hotmail.com>
2021-04-17 18:47:52 -0700

    Adds Header Component And Spin Effect on Emblem

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderStyles from '../styles/Header.module.scss';
+const Header = () => {
+  return (
+    <div className={HeaderStyles.container}>
+      <p className={HeaderStyles.content}>
+        Hello, my name is <span>Quinten Aiton.</span> <br />I am a full-stack
+        developer with a passion for innovative design and green tech.
+      </p>
+    </div>
+  );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Main from '../styles/Home.module.scss';
 import Nav from '../components/Nav';
+import Header from '../components/Header';
 const Index = () => {
   return (
     <div className={Main.container}>
-      <Nav />
+      <Nav className={Main.nav} />
+      <Header className={Main.header} />
     </div>
   );
 };
