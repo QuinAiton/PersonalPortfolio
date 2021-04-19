d68c177e9c2784551b7f5ceaaf17eace4f8a7e21
quinaiton <quinaiton@hotmail.com>
2021-04-19 08:19:01 -0700

    Fixes Spacing of Header Component

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,22 @@
 import React, { useEffect } from 'react';
-import HeaderStyles from '../styles/Header.module.scss';
+import Styles from '../styles/Header.module.scss';
 const Header = () => {
   return (
-    <div className={HeaderStyles.container}>
-      <div className={HeaderStyles.typing}>
+    <div className={Styles.container}>
+      <div className={Styles.typing}>
         <p>
           Hi, My Name is <span>Quinten Aiton.</span>
         </p>
         <p>I am a full stack developer.</p>
-        <p>I Like Creating things for the Web and green tech.</p>
+        <p>
+          I Love technology with a purpose and helping bring peoples ideas to
+          life.
+        </p>
       </div>
-      <p className={HeaderStyles.explore}> Explore </p>
-      <div className={HeaderStyles.arrow} />
+      <div className={Styles.explore}>
+        <p> Explore </p>
+        <div className={Styles.arrow} />
+      </div>
     </div>
   );
 };
