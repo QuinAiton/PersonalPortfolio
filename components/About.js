02d70c6fdb76be5d34cdae61036d971a44d2587c
quinaiton <quinaiton@hotmail.com>
2021-04-19 08:21:30 -0700

    Adds Component and Style Skeleton

---
+++ components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Styles from '../styles/About.module.scss';
+const About = () => {
+  return (
+    <div className={Styles.container}>
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default About;
