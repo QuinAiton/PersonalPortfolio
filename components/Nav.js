7f11928fce372b1c31812f491e8bb28d7df06d9a
quinaiton <quinaiton@hotmail.com>
2021-04-24 17:55:20 -0700

    Adds Media Queries for Responsive Menu

--- components/Nav.js
+++ components/Nav.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavStyles from '../styles/Nav.module.scss';
 import { FaLinkedin, FaEnvelope } from 'react-icons/fa';
 import { VscGithub } from 'react-icons/vsc';
 import Fade from 'react-reveal/Fade';
 import Image from 'next/image';
 import Link from 'next/link';
+import Media from 'react-med';
 const Nav = () => {
+  const [isOpen, setOpen] = useState(false);
   return (
     <Fade top>
-      <div className={NavStyles.container}>
-        <div className={NavStyles.items}>
+      {isOpen ? (
+        <nav className={NavStyles.container}>
           <Image
             className={NavStyles.image}
             src='/logo.png'
@@ -17,37 +19,44 @@ const Nav = () => {
             height={130}
             width={130}
           />
-          <ul className={NavStyles.links}>
+          <div className={NavStyles.items}>
+            <ul className={NavStyles.links}>
+              <li>
+                <Link href='#about'>
+                  <a>About</a>
+                </Link>
+              </li>
+              <li>
+                <Link href='#projects'>
+                  <a>Projects</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <ul className={NavStyles.contact}>
             <li>
-              <Link href='#about'>
-                <a>About</a>
-              </Link>
+              <a href='https://github.com/quinaiton' target='_blank'>
+                <VscGithub className={NavStyles.icon} />
+              </a>
             </li>
             <li>
-              <Link href='#projects'>
-                <a>Projects</a>
-              </Link>
+              <a
+                href='https://www.linkedin.com/in/quinten-aiton'
+                target='_blank'
+              >
+                <FaLinkedin className={NavStyles.icon} />
+              </a>
+            </li>
+            <li>
+              <a href='mailto:quinaiton@pm.me' target='_blank'>
+                <FaEnvelope className={NavStyles.icon} />
+              </a>
             </li>
           </ul>
-        </div>
-        <ul className={NavStyles.contact}>
-          <li>
-            <a href='https://github.com/quinaiton' target='_blank'>
-              <VscGithub className={NavStyles.icon} />
-            </a>
-          </li>
-          <li>
-            <a href='https://www.linkedin.com/in/quinten-aiton' target='_blank'>
-              <FaLinkedin className={NavStyles.icon} />
-            </a>
-          </li>
-          <li>
-            <a href='mailto:quinaiton@pm.me' target='_blank'>
-              <FaEnvelope className={NavStyles.icon} />
-            </a>
-          </li>
-        </ul>
-      </div>
+        </nav>
+      ) : (
+        <VscGithub onClick={() => setOpen(true)} />
+      )}
     </Fade>
   );
 };
